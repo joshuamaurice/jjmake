1a64ca3ddcbf1275421655f722ea5fa0c9f714e5
Joshua Maurice <joshuamaurice@gmail.com>
2015-07-05 15:02:34 -0700

    initial commit, skeleton main

---
+++ jjmake/main.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2010-2011, Informatica Corporation, Joshua Maurice
+//       Distributed under the 3-clause BSD License
+//      (See accompanying file LICENSE.TXT or copy at
+//  http://www.w3.org/Consortium/Legal/2008/03-bsd-license.html)
+
+
+
+#ifdef _WIN32
+int wmain(int argc, wchar_t **argv)
+{
+}
+#else
+int main(int argc, char** argv)
+{
+}
+#endif
